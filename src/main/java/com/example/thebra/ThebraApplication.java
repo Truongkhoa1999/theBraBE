2f88017c432f3f8dfb27ee5e3c1a579c533cf065
Truongkhoa1999 <tdkhoa.aqua@gmail.com>
2023-08-02 17:33:06 +0300

    working on Heroku

--- src/main/java/com/example/thebra/ThebraApplication.java
+++ src/main/java/com/example/thebra/ThebraApplication.java
@@ -2,10 +2,11 @@ package com.example.thebra;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
+@PropertySource("classpath:application.properties")
 public class ThebraApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(ThebraApplication.class, args);
 	}
