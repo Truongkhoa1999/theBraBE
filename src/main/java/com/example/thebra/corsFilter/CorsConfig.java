6321a49f630413571477b8f4f04fb0840d81f6ec
Khoa <tdkhoa.aqua@gmail.com>
2023-08-14 21:51:08 +0300

    6

--- src/main/java/com/example/thebra/corsFilter/CorsConfig.java
+++ src/main/java/com/example/thebra/corsFilter/CorsConfig.java
@@ -9,10 +9,11 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:5173","*")
+                .allowedOrigins("*")
                 .allowedMethods("GET", "POST", "PUT", "DELETE")
                 .allowedHeaders("*")
                 .allowCredentials(true)
                 .maxAge(3600);
+//        "http://localhost:5173"
     }
 }
