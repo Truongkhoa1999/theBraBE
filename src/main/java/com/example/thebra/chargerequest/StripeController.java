18797146d493b4a7baba1ad75842abd995f06af1
Khoa <tdkhoa.aqua@gmail.com>
2023-08-16 20:50:15 +0300

    15a

--- src/main/java/com/example/thebra/chargerequest/StripeController.java
+++ src/main/java/com/example/thebra/chargerequest/StripeController.java
@@ -17,7 +17,7 @@ import java.util.UUID;
 @RestController
 @RequestMapping("/api/v1/stripe")
 public class StripeController {
-    private final String secretKey = "sk_test_51NWsLeKEPicYF8bFnAdKL6QGW7yPJJBLrZLGbswc6VDMWJfH3TdrMnp73Jjn7OsKFTYsiqVOTmtmOfOWztqrGVtl00hA9oXTPH";  // Replace with your actual secret key
+    private final String secretKey = "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";  // Replace with your actual secret key
 
     @Autowired
     private StripeService stripeService;
--- src/main/java/com/example/thebra/chargerequest/StripeService.java
+++ src/main/java/com/example/thebra/chargerequest/StripeService.java
@@ -17,7 +17,7 @@ import java.util.Map;
 @Service
 public class StripeService {
 
-    private final String secretKey = "sk_test_51NWsLeKEPicYF8bFnAdKL6QGW7yPJJBLrZLGbswc6VDMWJfH3TdrMnp73Jjn7OsKFTYsiqVOTmtmOfOWztqrGVtl00hA9oXTPH";  // Replace with your actual secret key
+    private final String secretKey = "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";  // Replace with your actual secret key
 
     @Autowired
     private OrderService orderService;
