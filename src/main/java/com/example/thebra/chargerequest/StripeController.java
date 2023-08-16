5cc6196471c08a16cb958ab3303c4d8c3873f85d
Khoa <tdkhoa.aqua@gmail.com>
2023-08-16 12:32:00 +0300

    14d

--- src/main/java/com/example/thebra/chargerequest/StripeController.java
+++ src/main/java/com/example/thebra/chargerequest/StripeController.java
@@ -2,9 +2,12 @@ package com.example.thebra.chargerequest;
 
 import com.example.thebra.order.Order;
 import com.example.thebra.order.OrderService;
+import com.stripe.Stripe;
 import com.stripe.exception.StripeException;
 import com.stripe.model.Charge;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.ui.Model; // Correct import for the Model class
 
@@ -12,36 +15,47 @@ import java.util.UUID;
 
 
 @RestController
-@RequestMapping("api/v1/stripe")
 public class StripeController {
-
+    @Value("${STRIPE_SECRET_KEY}")
+    private String secretKey;
     @Autowired
     private StripeService stripeService;
 
     @Autowired
     private OrderService orderService;
 
-    @PostMapping("/charge")
-    public String charge(@RequestBody ChargeRequest chargeRequest, Model model)
-            throws StripeException {
-        UUID orderId = chargeRequest.getOrderId();
-         Order order = orderService.findOrderById(orderId);
-         order.setPaymentStatus("Paid");
-//        Make Stripe request
-        chargeRequest.setCurrency(ChargeRequest.Currency.EUR);
+    //    @PostMapping("api/v1/stripe/charge")
+//    public String charge(@RequestBody ChargeRequest chargeRequest, Model model)
+//            throws StripeException {
+//        UUID orderId = chargeRequest.getOrderId();
+//        Order order = orderService.findOrderById(orderId);
+//        order.setPaymentStatus("Paid");
+////        Make Stripe request
+//        chargeRequest.setCurrency(ChargeRequest.Currency.EUR);
+//        Charge charge = stripeService.charge(chargeRequest);
+//        if (charge != null) {
+//            model.addAttribute("id", charge.getId());
+//            model.addAttribute("status", charge.getStatus());
+//            model.addAttribute("chargeId", charge.getId());
+//            model.addAttribute("balance_transaction", charge.getBalanceTransaction());
+//            return "result";
+//        } else {
+//            model.addAttribute("error", "Payment failed. Please try again.");
+//            return "result";
+//        }
+//    }
+    @PostMapping("/api/v1/stripe/charge")
+    public String charge(@RequestBody ChargeRequest chargeRequest) throws StripeException {
+        Stripe.apiKey = secretKey;
+
+        // Create a charge using Stripe API
         Charge charge = stripeService.charge(chargeRequest);
+
         if (charge != null) {
-            model.addAttribute("id", charge.getId());
-            model.addAttribute("status", charge.getStatus());
-            model.addAttribute("chargeId", charge.getId());
-            model.addAttribute("balance_transaction", charge.getBalanceTransaction());
-        return "result";
-        }     else{
-            model.addAttribute("error", "Payment failed. Please try again.");
-            return "result";
+            return "Payment successful!";
+        } else {
+            return "Payment failed.";
         }
-
-
     }
 
     @ExceptionHandler(StripeException.class)
--- src/main/java/com/example/thebra/chargerequest/StripeService.java
+++ src/main/java/com/example/thebra/chargerequest/StripeService.java
@@ -1,12 +1,17 @@
 package com.example.thebra.chargerequest;
 
+import com.example.thebra.order.Order;
+import com.example.thebra.order.OrderService;
 import com.stripe.Stripe;
 import com.stripe.exception.*;
 
 import com.stripe.model.Charge;
 import jakarta.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+
+import java.util.UUID;
 
 
 import javax.naming.AuthenticationException;
@@ -18,25 +23,44 @@ public class StripeService {
 
     @Value("${STRIPE_SECRET_KEY}")
     private String secretKey;
+    @Value("${STRIPE_SECRET_KEY}") // Inject your Stripe secret key from application.properties
+    private String stripeSecretKey;
+    @Autowired
+    private OrderService orderService;
 
     @PostConstruct
     public void init() {
         System.out.println("Stripe Secret Key: " + secretKey); // Print the secret key
         Stripe.apiKey = secretKey;
     }
-    public Charge charge(ChargeRequest chargeRequest) {
-        try {
-            System.out.println("Total Amount: " + chargeRequest.getAmount()); // Add this line for logging
 
-            Map<String, Object> chargeParams = new HashMap<>();
-            chargeParams.put("amount", chargeRequest.getAmount());
-            chargeParams.put("currency", chargeRequest.getCurrency());
-            chargeParams.put("source", chargeRequest.getStripeToken());
-            return Charge.create(chargeParams);
-        } catch (  StripeException e  ) {
-            e.printStackTrace();
-            return null;
-        }
+    //    public Charge charge(ChargeRequest chargeRequest) {
+//        try {
+//            System.out.println("Total Amount: " + chargeRequest.getAmount()); // Add this line for logging
+//
+//            Map<String, Object> chargeParams = new HashMap<>();
+//            chargeParams.put("amount", chargeRequest.getAmount());
+//            chargeParams.put("currency", chargeRequest.getCurrency());
+//            chargeParams.put("source", chargeRequest.getStripeToken());
+//            return Charge.create(chargeParams);
+//        } catch (  StripeException e  ) {
+//            e.printStackTrace();
+//            return null;
+//        }
+//    }
+    public Charge charge(ChargeRequest chargeRequest) throws StripeException {
+        Stripe.apiKey = stripeSecretKey;
+
+        // Create a charge using Stripe API
+        Charge charge = Charge.create(Map.of(
+                "amount", chargeRequest.getAmount(),
+                "currency", chargeRequest.getCurrency(),
+                "source", chargeRequest.getStripeToken()
+        ));
+        UUID orderId = chargeRequest.getOrderId();
+        Order order = orderService.findOrderById(orderId);
+        order.setPaymentStatus("Paid");
+        return charge;
     }
 
 }
