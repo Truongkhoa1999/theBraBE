0b7cf9dbb934f6a8e05885d983bc8354439b8b0f
Khoa <tdkhoa.aqua@gmail.com>
2023-08-21 18:53:14 +0300

    19master

--- src/main/java/com/example/thebra/corsFilter/CorsConfig.java
+++ src/main/java/com/example/thebra/corsFilter/CorsConfig.java
@@ -9,8 +9,10 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("https://64e38282e0140d2c0944967b--ubiquitous-kataifi-98ab0e.netlify.app")
-                .allowedOrigins("http://localhost:5173/")
+                .allowedOrigins
+                        (
+                                "https://64e38282e0140d2c0944967b--ubiquitous-kataifi-98ab0e.netlify.app",
+                                "http://localhost:5173")
                 .allowedMethods("GET", "POST", "PUT", "DELETE")
                 .allowedHeaders("*")
                 .allowCredentials(true)
