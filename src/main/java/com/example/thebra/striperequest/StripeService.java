bec6463559a7dd220a42e950c28d8711761741ef
Khoa <tdkhoa.aqua@gmail.com>
2023-08-29 02:00:43 +0300

    18am

--- src/main/java/com/example/thebra/striperequest/StripeService.java
+++ src/main/java/com/example/thebra/striperequest/StripeService.java
@@ -7,43 +7,7 @@ import com.stripe.param.PaymentIntentCreateParams;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
-//
-//import com.example.thebra.order.OrderService;
-//import com.stripe.Stripe;
-//import com.stripe.exception.*;
-//
-//import com.stripe.model.Charge;
-//import jakarta.annotation.PostConstruct;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.stereotype.Service;
-//
-//import java.util.Map;
-//
-//@Service
-//public class StripeService {
-//
-//    private final String secretKey = "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";  // Replace with your actual secret key
-//
-//    @Autowired
-//    private OrderService orderService;
-//
-//    @PostConstruct
-//    public void init() {
-//        System.out.println("Stripe Secret Key: " + secretKey); // Print the secret key
-//        Stripe.apiKey = secretKey;
-//    }
-//    public Charge charge(StripeRequest chargeRequest) throws StripeException {
-//        Stripe.apiKey = secretKey;
-//        // Create a charge using Stripe API
-//        Charge charge = Charge.create(Map.of(
-//                "amount", chargeRequest.getAmount(),
-//                "currency", chargeRequest.getCurrency(),
-//                "source", chargeRequest.getStripeToken()
-//        ));
-//        return charge;
-//    }
-//
-//}
+
 @Service
 public class StripeService {
 
--- src/main/java/com/example/thebra/stripewebhook/IpFilter.java
+++ src/main/java/com/example/thebra/stripewebhook/IpFilter.java
@@ -1,50 +1,50 @@
-package com.example.thebra.stripewebhook;
-
-import jakarta.servlet.FilterChain;
-import jakarta.servlet.ServletException;
-import jakarta.servlet.ServletRequest;
-import jakarta.servlet.ServletResponse;
-import jakarta.servlet.http.HttpServletRequest;
-import org.springframework.stereotype.Component;
-import org.springframework.web.filter.GenericFilterBean;
-
-import java.io.IOException;
-import java.util.Arrays;
-import java.util.List;
-
-@Component
-public class IpFilter extends GenericFilterBean {
-
-    private final List<String> allowedIpAddresses = Arrays.asList(
-            "3.18.12.63",
-            "3.130.192.231",
-            "13.235.14.237"
-            ,"13.235.122.149",
-            "18.211.135.69",
-            "35.154.171.200",
-            "52.15.183.38",
-            "54.88.130.119",
-            "54.88.130.237",
-            "54.187.174.169",
-            "54.187.205.235",
-            "54.187.216.72"
-            );
-
-    @Override
-    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
-        if (isStripeWebhookEndpoint(request)) {
-            String ipAddress = request.getRemoteAddr();
-            if (!allowedIpAddresses.contains(ipAddress)) {
-                response.getWriter().write("Access Denied"); // Customize your response here
-                return;
-            }
-        }
-
-        chain.doFilter(request, response);
-    }
-    private boolean isStripeWebhookEndpoint(ServletRequest request) {
-        // Check if the request is targeting the Stripe webhook endpoint
-        String requestURI = ((HttpServletRequest) request).getRequestURI();
-        return requestURI.equals("/webhooks/payment-intent-succeeded");
-    }
-}
+//package com.example.thebra.stripewebhook;
+//
+//import jakarta.servlet.FilterChain;
+//import jakarta.servlet.ServletException;
+//import jakarta.servlet.ServletRequest;
+//import jakarta.servlet.ServletResponse;
+//import jakarta.servlet.http.HttpServletRequest;
+//import org.springframework.stereotype.Component;
+//import org.springframework.web.filter.GenericFilterBean;
+//
+//import java.io.IOException;
+//import java.util.Arrays;
+//import java.util.List;
+//
+//@Component
+//public class IpFilter extends GenericFilterBean {
+//
+//    private final List<String> allowedIpAddresses = Arrays.asList(
+//            "3.18.12.63",
+//            "3.130.192.231",
+//            "13.235.14.237"
+//            ,"13.235.122.149",
+//            "18.211.135.69",
+//            "35.154.171.200",
+//            "52.15.183.38",
+//            "54.88.130.119",
+//            "54.88.130.237",
+//            "54.187.174.169",
+//            "54.187.205.235",
+//            "54.187.216.72"
+//            );
+//
+//    @Override
+//    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+//        if (isStripeWebhookEndpoint(request)) {
+//            String ipAddress = request.getRemoteAddr();
+//            if (!allowedIpAddresses.contains(ipAddress)) {
+//                response.getWriter().write("Access Denied"); // Customize your response here
+//                return;
+//            }
+//        }
+//
+//        chain.doFilter(request, response);
+//    }
+//    private boolean isStripeWebhookEndpoint(ServletRequest request) {
+//        // Check if the request is targeting the Stripe webhook endpoint
+//        String requestURI = ((HttpServletRequest) request).getRequestURI();
+//        return requestURI.equals("/webhooks/payment-intent-succeeded");
+//    }
+//}
