5b227a703b82d5d3462afcea983f51c84df2f363
Khoa <tdkhoa.aqua@gmail.com>
2023-08-28 23:21:08 +0300

    18

--- src/main/java/com/example/thebra/securityconfig/SecurityConfig.java
+++ src/main/java/com/example/thebra/securityconfig/SecurityConfig.java
@@ -1,9 +1,7 @@
 package com.example.thebra.securityconfig;
 
 import com.example.thebra.filters.JwtFilter;
-import com.example.thebra.stripewebhook.IpFilter;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
@@ -20,7 +18,6 @@ import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
 public class SecurityConfig {
-
     @Autowired
     private JwtFilter jwtFilter;
 
