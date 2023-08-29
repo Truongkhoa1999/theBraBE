3edd1ced38784b63f9feddc0dedb1aa53a7b951f
Khoa <tdkhoa.aqua@gmail.com>
2023-08-29 20:42:30 +0300

    18sjuz

--- src/main/java/com/example/thebra/stripewebhook/StripeWebhookController.java
+++ src/main/java/com/example/thebra/stripewebhook/StripeWebhookController.java
@@ -1,57 +1,57 @@
-package com.example.thebra.stripewebhook;
-import com.example.thebra.order.Order;
-import com.example.thebra.order.OrderService;
-import com.stripe.exception.SignatureVerificationException;
-import com.stripe.model.Event;
-import com.stripe.model.PaymentIntent;
-import com.stripe.net.Webhook;
-import jakarta.servlet.http.HttpServletRequest;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-import java.util.UUID;
-import java.util.List;
-
-@RestController
-@RequestMapping("/webhooks")
-public class StripeWebhookController {
-    @Autowired
-    OrderService orderService;
-    private String webhookSecret = "whsec_VLM1U4dOSFEJZbbcIDrXCxuFgUkv9SYE";
-    @PostMapping("/payment-intent-succeeded")
-    public ResponseEntity<String> handlePaymentIntentSucceeded(
-            @RequestBody String payload,
-            @RequestHeader("Stripe-Signature") String stripeSignature,
-            HttpServletRequest request) {
-        try {
-            Event event = Webhook.constructEvent(payload, stripeSignature, webhookSecret);
-            if ("payment_intent.succeeded".equals(event.getType())) {
-                PaymentIntent paymentIntent = (PaymentIntent) event.getDataObjectDeserializer().getObject().orElse(null);
-                // Retrieve order_id from metadata
-                String orderId = paymentIntent.getMetadata().get("order_id");
-                Order detectOrder = orderService.findOrderById(UUID.fromString(orderId));
-                if (detectOrder != null) {
-                    if ("pending".equals(detectOrder.getPaymentStatus())) {
-                        detectOrder.setPaymentStatus("paid");
-                        System.out.println("Payment status changed to paid");
-                    }
-                } else {
-                    System.out.println("Order not found, payment status not changed");
-                }
-                return ResponseEntity.ok("Webhook handled successfully");
-            }
-        } catch (SignatureVerificationException e) {
-            // Invalid signature
-            System.out.println(e);
-            return ResponseEntity.badRequest().build();
-        } catch (Exception e) {
-            // Handle other exceptions
-            System.out.println(e);
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
-        }
-        return ResponseEntity.ok("Webhook event ignored");
-    }
-
-}
+//package com.example.thebra.stripewebhook;
+//import com.example.thebra.order.Order;
+//import com.example.thebra.order.OrderService;
+//import com.stripe.exception.SignatureVerificationException;
+//import com.stripe.model.Event;
+//import com.stripe.model.PaymentIntent;
+//import com.stripe.net.Webhook;
+//import jakarta.servlet.http.HttpServletRequest;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.beans.factory.annotation.Value;
+//import org.springframework.http.HttpStatus;
+//import org.springframework.http.ResponseEntity;
+//import org.springframework.web.bind.annotation.*;
+//import java.util.UUID;
+//import java.util.List;
+//
+//@RestController
+//@RequestMapping("/webhooks")
+//public class StripeWebhookController {
+//    @Autowired
+//    OrderService orderService;
+//    private String webhookSecret = "whsec_VLM1U4dOSFEJZbbcIDrXCxuFgUkv9SYE";
+//    @PostMapping("/payment-intent-succeeded")
+//    public ResponseEntity<String> handlePaymentIntentSucceeded(
+//            @RequestBody String payload,
+//            @RequestHeader("Stripe-Signature") String stripeSignature,
+//            HttpServletRequest request) {
+//        try {
+//            Event event = Webhook.constructEvent(payload, stripeSignature, webhookSecret);
+//            if ("payment_intent.succeeded".equals(event.getType())) {
+//                PaymentIntent paymentIntent = (PaymentIntent) event.getDataObjectDeserializer().getObject().orElse(null);
+//                // Retrieve order_id from metadata
+//                String orderId = paymentIntent.getMetadata().get("order_id");
+//                Order detectOrder = orderService.findOrderById(UUID.fromString(orderId));
+//                if (detectOrder != null) {
+//                    if ("pending".equals(detectOrder.getPaymentStatus())) {
+//                        detectOrder.setPaymentStatus("paid");
+//                        System.out.println("Payment status changed to paid");
+//                    }
+//                } else {
+//                    System.out.println("Order not found, payment status not changed");
+//                }
+//                return ResponseEntity.ok("Webhook handled successfully");
+//            }
+//        } catch (SignatureVerificationException e) {
+//            // Invalid signature
+//            System.out.println(e);
+//            return ResponseEntity.badRequest().build();
+//        } catch (Exception e) {
+//            // Handle other exceptions
+//            System.out.println(e);
+//            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+//        }
+//        return ResponseEntity.ok("Webhook event ignored");
+//    }
+//
+//}
